7b2853f2178a9e45ed9fd81a9c1265f84ffa3db5
Michael Steiner <michaelvsteiner@gmail.com>
2021-08-20 13:14:31 -0600

    Work on submitting and posting form, create codegrade submission

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { Switch, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import PizzaForm from "./PizzaForm";
 import Order from "./Order";
+import axios from "axios";
 
 const initialSelections = {
   name: "",
@@ -18,14 +19,43 @@ const initialSelections = {
   extraCheese: false,
 };
 
+const initialOrders = [];
+
 export default function App() {
+  const [orders, setOrders] = useState(initialOrders);
   const [selections, setSelections] = useState(initialSelections);
+
+  const getOrder = () => {
+    axios.get();
+  };
+
+  const postNewOrder = (newOrder) => {
+    axios.post(`https://reqres.in/api/orders`, newOrder).then((res) => {
+      // setFriends([res.?, ...orders])
+    });
+  };
 
   const inputChange = (name, value) => {
     // validate(name, value);
     setSelections({ ...selections, [name]: value });
   };
-  console.log(selections);
+
+  const formSubmit = () => {
+    const newOrder = {
+      name: selections.name,
+      size: selections.size,
+      pepperoni: selections.pepperoni,
+      sausage: selections.sausage,
+      canadianBacon: selections.canadianBacon,
+      spicyItalianSausage: selections.spicyItalianSausage,
+      onion: selections.onion,
+      greenPeppers: selections.greenPeppers,
+      blackOlives: selections.blackOlives,
+      extraCheese: selections.extraCheese,
+    };
+    postNewOrder(newOrder);
+  };
+
   return (
     <div>
       <nav>
